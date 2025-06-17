0ab0c836205af61a7a8fd6e0f9db9f46a9f1da9f
Rupesh Koirala <38342445+RupeshKoirala@users.noreply.github.com>
2025-06-16 23:32:32 -0400

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,7 +2,7 @@ export default function ContactPage() {
   return (
     <div className="h-screen w-full">
       <iframe
-        data-tally-src="https://tally.so/r/3NgP2Q?transparentBackground=1"
+        data-tally-src="https://tally.so/r/m6GKkO?transparentBackground=1"
         width="100%"
         height="100%"
         title="Contact Form"
